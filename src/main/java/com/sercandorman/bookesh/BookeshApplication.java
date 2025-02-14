cff2057ee5752d22ecd3d1f4580b33dacadb6eb6
Sercan Dorman <sercandormanusa@gmail.com>
2025-02-13 22:29:42 -0800

    ilk commit

---
+++ src/main/java/com/sercandorman/bookesh/BookeshApplication.java
@@ -0,0 +1,13 @@
+package com.sercandorman.bookesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sercandorman/bookesh/BookeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sercandorman.bookesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
